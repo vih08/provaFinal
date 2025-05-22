eeb11818f77036e8548dc2510b0ac8b396dd5073
VITÓRIA MARINI DA SILVA <vitoria_silva18@estudante.sesisenai.org.br>
2025-05-22 09:51:05 -0300

    função do log, tentativa k

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const express = require('express')
+const PORTA = 8000
 
+function registrarLog(nomeAluno){
+  const idUnico = uuidv4()
+  const dataHora = new Date().toLocaleString
+  const mensagem = `${idUnico} - ${dataHora} - ${nomeAluno}\``
+
+
+  fs.appendFileSync("log registrado")
+  return idUnico
+}
