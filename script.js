3cd2c8a8fcdfac7dd898534377258df005feee80
VITÓRIA MARINI DA SILVA <vitoria_silva18@estudante.sesisenai.org.br>
2025-05-22 11:06:14 -0300

    deletando node modules

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+//iniciei projeto na pasta vazia e depois começei a base 
+
 const express = require('express')
 const PORTA = 8000
 
+// passo 1 o registro do log
 function registrarLog(nomeAluno){
   const idUnico = uuidv4()
   const dataHora = new Date().toLocaleString
@@ -11,16 +14,19 @@ function registrarLog(nomeAluno){
   return idUnico
 }
 
+//passo 2 o express
 const express = require("express")
 const app = express()
 
 app.use(express.json())
 
+
+//passo 3
 const PORT = 8000
 app.listen(PORT, ()=>{
   console.log(`é pra estar rodando na porta ${8000}`)
 })
-
+//passo 4
 app.post('/logs', (req,res)=>{
   const{nomeAluno}=req.body
 
@@ -28,4 +34,17 @@ app.post('/logs', (req,res)=>{
     return res.status(400).json({erro:"nome do aluno é obrigatorio por"})
   }const idGerado = registrarLog(nome)
   res.json({id:idGerado, mensagem:"log gerado (eu espero na vdd)"})
+})
+
+//passo 5
+
+const fs = require("fs")
+
+app.get("/logs/:id", (req, res) =>{
+  const idBuscando = req.params.id
+if (logEncontrado){
+  res.json({mensagem: logEncontrado })
+}else {
+  res.status(404).json({erro:"log não encontrado"})
+}
 })
