47da4d1de903a24510ef8d224e1712ae01234c04
VITÓRIA MARINI DA SILVA <vitoria_silva18@estudante.sesisenai.org.br>
2025-05-22 10:07:14 -0300

    rota para add o log

--- script.js
+++ script.js
@@ -21,4 +21,11 @@ app.listen(PORT, ()=>{
   console.log(`é pra estar rodando na porta ${8000}`)
 })
 
+app.post('/logs', (req,res)=>{
+  const{nomeAluno}=req.body
 
+  if(!nomeAluno){
+    return res.status(400).json({erro:"nome do aluno é obrigatorio por"})
+  }const idGerado = registrarLog(nome)
+  res.json({id:idGerado, mensagem:"log gerado (eu espero na vdd)"})
+})
